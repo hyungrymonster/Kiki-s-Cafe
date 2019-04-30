33c9b810a91c022eb69327681651b8f6dcb36d28
Hyun Yoo <hj03yoo@gmail.com>
2019-04-30 18:31:52 -0400

    kiki page

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function mobileNav() {
+  var x = document.getElementById("mobileNavMenus");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
